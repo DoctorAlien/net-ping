d877389e76d5c102e06f9cca7677d443d0319ffc
DoctorAlien <shenjiajun2009@126.com>
2016-03-17 12:36:12 +0800

    Ping
    Ping/IP

---
+++ net-ping/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace net_ping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtHostname = new System.Windows.Forms.TextBox();
+            this.btnClick = new System.Windows.Forms.Button();
+            this.listIPs = new System.Windows.Forms.ListBox();
+            this.listPing = new System.Windows.Forms.ListBox();
+            this.combNum = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnPing = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 40);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(300, 25);
+            this.txtInput.TabIndex = 0;
+            // 
+            // txtHostname
+            // 
+            this.txtHostname.Location = new System.Drawing.Point(12, 110);
+            this.txtHostname.Name = "txtHostname";
+            this.txtHostname.Size = new System.Drawing.Size(300, 25);
+            this.txtHostname.TabIndex = 1;
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(333, 80);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(300, 55);
+            this.btnClick.TabIndex = 2;
+            this.btnClick.Text = "确定";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // listIPs
+            // 
+            this.listIPs.FormattingEnabled = true;
+            this.listIPs.ItemHeight = 15;
+            this.listIPs.Location = new System.Drawing.Point(12, 159);
+            this.listIPs.Name = "listIPs";
+            this.listIPs.Size = new System.Drawing.Size(300, 229);
+            this.listIPs.TabIndex = 3;
+            // 
+            // listPing
+            // 
+            this.listPing.FormattingEnabled = true;
+            this.listPing.ItemHeight = 15;
+            this.listPing.Location = new System.Drawing.Point(333, 159);
+            this.listPing.Name = "listPing";
+            this.listPing.Size = new System.Drawing.Size(765, 229);
+            this.listPing.TabIndex = 4;
+            // 
+            // combNum
+            // 
+            this.combNum.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.combNum.FormattingEnabled = true;
+            this.combNum.Location = new System.Drawing.Point(333, 40);
+            this.combNum.Name = "combNum";
+            this.combNum.Size = new System.Drawing.Size(300, 23);
+            this.combNum.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "请输入IP地址";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "源地址";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(330, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Ping次数";
+            // 
+            // btnPing
+            // 
+            this.btnPing.Location = new System.Drawing.Point(653, 40);
+            this.btnPing.Name = "btnPing";
+            this.btnPing.Size = new System.Drawing.Size(445, 95);
+            this.btnPing.TabIndex = 9;
+            this.btnPing.Text = "Ping";
+            this.btnPing.UseVisualStyleBackColor = true;
+            this.btnPing.Click += new System.EventHandler(this.btnPing_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1110, 400);
+            this.Controls.Add(this.btnPing);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.combNum);
+            this.Controls.Add(this.listPing);
+            this.Controls.Add(this.listIPs);
+            this.Controls.Add(this.btnClick);
+            this.Controls.Add(this.txtHostname);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Net-Ping";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtHostname;
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.ListBox listIPs;
+        private System.Windows.Forms.ListBox listPing;
+        private System.Windows.Forms.ComboBox combNum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnPing;
+    }
+}
+
---
+++ net-ping/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace net_ping
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            listIPs.Items.Clear();
+            try
+            {
+                //IP地址
+                IPHostEntry iphost = Dns.GetHostEntry(txtInput.Text);
+                foreach (IPAddress ip in iphost.AddressList) {
+                    string ipaddress = ip.AddressFamily.ToString();
+                    listIPs.Items.Add(ipaddress);
+                    listIPs.Items.Add(" "+ip.ToString());
+                }
+                txtHostname.Text = iphost.HostName;
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("不能连接至服务器，由于以下原因:\n"+ex.Message,"Error");
+                throw;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            combNum.Items.Add("1");
+            combNum.Items.Add("5");
+            combNum.Items.Add("10");
+            combNum.Items.Add("15");
+            combNum.Items.Add("20");
+            combNum.Text = combNum.Items[0].ToString();
+
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+        }
+
+        private void btnPing_Click(object sender, EventArgs e)
+        {
+            listPing.Items.Clear();
+            string strNum = combNum.Text;
+            int intNum = int.Parse(strNum);
+            try
+            {
+                //Ping
+                for (int i = 0; i < intNum; i++)
+                {
+                    Ping p1 = new Ping();
+                    PingReply reply = p1.Send(txtInput.Text);
+                    StringBuilder sbuilder;
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        sbuilder = new StringBuilder();
+                        sbuilder.Append(string.Format("地址: {0}  ", reply.Address.ToString()));
+                        sbuilder.Append(string.Format("时间: {0}ms  ", reply.RoundtripTime));
+                        sbuilder.Append(string.Format("TTL: {0}  ", reply.Options.Ttl));
+                        sbuilder.Append(string.Format("字节: {0}  ", reply.Buffer.Length));
+                        listPing.Items.Add(sbuilder.ToString());
+                    }
+                    else if (reply.Status == IPStatus.TimedOut)
+                    {
+                        listPing.Items.Add("超时");
+                    }
+                    else
+                    {
+                        listPing.Items.Add("失败");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("瞬间爆炸！！！");
+            }
+        }
+    }
+}
